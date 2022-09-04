8949346ad343070993517dde4db5c0f6bd77b029
Abhay Sinha <abhay.sinha@mdally.com>
2022-09-04 14:51:09 -0400

    Initial Commit

---
+++ cplus-cli/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  cplus-cli
+//
+//  Created by Abhay Sinha on 9/4/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
