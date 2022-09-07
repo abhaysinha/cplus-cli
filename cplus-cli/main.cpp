e312633c148113efcc0caabd884f03772c1a3a31
Abhay Sinha <abhay.sinha@mdally.com>
2022-09-06 21:54:58 -0400

    branch-1

--- cplus-cli/main.cpp
+++ cplus-cli/main.cpp
@@ -6,9 +6,16 @@
 //
 
 #include <iostream>
+#include <string>
+#include "my_logger.hpp"
+
+using namespace std;
 
 int main(int argc, const char * argv[]) {
+    int i = 203;
+    string s = to_string(i);
     // insert code here...
-    std::cout << "Hello, World!\n";
+    std::cout << s << " Hello, World!\n";
+    log("Let's log this.");
     return 0;
 }
---
+++ cplus-cli/my_logger.hpp
@@ -0,0 +1,16 @@
+//
+//  my_logger.hpp
+//  cplus-cli
+//
+//  Created by Abhay Sinha on 9/6/22.
+//
+
+#ifndef my_logger_hpp
+#define my_logger_hpp
+
+#include <stdio.h>
+#include <string>
+
+void log(std::string);
+
+#endif /* my_logger_hpp */
